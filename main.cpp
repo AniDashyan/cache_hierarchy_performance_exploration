562d59716fd00a9615bc92b0624c7756d557370d
Ani Dashyan <adashyan@griddynamics.com>
2025-02-26 06:13:10 -0800

    format output

--- main.cpp
+++ main.cpp
@@ -7,32 +7,38 @@
 
 using namespace std::chrono;
 
-const int size = 16 * 256 * 1024;
+const int size = 16 * 256 * 1024 * 64;
 std::vector<int> strides = {1, 4, 16, 32, 64, 256, 1024};
 
 template <typename T>
 auto access_time(T& cont, int stride) {
+    int sum = 0;
+    auto it = cont.begin();
+
     auto start = steady_clock::now();
-    for (int i = 0; i < size; i++) {
-    
+    while (it != cont.end()) {
+        sum += *it;
+        std::advance(it, stride);
     }
     auto end = steady_clock::now();
     return (duration_cast<milliseconds>(end - start).count());
 }
 
-template <typename T>
-void print_output(T& cont, const std::string& c_name) {
-    for (size_t i = 0; i < strides.size(); i++) {
-        cout << c_name << " stride " << strides[i] << ": "
-            << access_time(cont, strides[i]) << " ms\n";
-    }
-}
 
 int main() {
     std::vector<int> vec(size);
     std::list<int> list(size);
 
-    print_output(vec, "Vector");
-    print_output(list, "List");
+    std::cout << std::left << std::setw(5) << "Stride" 
+              << std::right << std::setw(10) << "Vector" 
+              << std::right << std::setw(10) << "List" << "\n";
+    std::cout << std::string(30, '-') << "\n"; 
+
+
+    for (size_t i = 0; i < strides.size(); i++) {
+        std::cout << std::right << std::setw(5) << strides[i]
+                  << std::right << std::setw(10) << access_time(vec, strides[i]) << " ms"
+                  << std::right << std::setw(10) << access_time(list, strides[i]) << " ms\n";
+    }
     return (0);
 }
